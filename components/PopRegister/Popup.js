55bcd0efebb59424aeb2af8e2b0e8d84d4caa098
Muhammad Asim Shakoor <asimshakoor920830@gmail.com>
2024-05-22 17:04:21 +0500

    Update Popup.js

--- components/PopRegister/Popup.js
+++ components/PopRegister/Popup.js
@@ -1,54 +1,37 @@
 import React, { useEffect, useState } from 'react'
 
-export const Popup = ({ details, closeModal }) => {
+export const Popup = ({ details, event, closeModal }) => {
+  const { img, title, venue, date, desc, ticket } = event;
+
 
   return (
     <>
 
       <div className={`fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center ${details}`} style={{ zIndex: "9999" }}>
         {details === "unhide" && (
-          <div className="bg-white flex flex-col justify-between rounded-lg py-8 w-[96vw] h-[96vh]">
+          <div className="bg-white flex flex-col justify-between rounded-lg py-8 w-[90vw] h-[90vh]">
             <div id="modal-table">
-              <div className="container mx-auto px-0">
+              <div className="container mx-auto px-2">
                 <div className="overflow-x-auto">
+                  <div className="font-bold text-[18px] mb-2 text-left">
+                    Event: {title}
+                  </div>
+
                   <table className="table-auto w-full text-left">
                     <thead>
-                      <tr className="bg-zinc-100  text-[18px]">
-                        <th className="px-4 py-2">Detail</th>
-                        <th className="px-4 py-2">Winner</th>
-                        <th className="px-4 py-2">RunnerUp</th>
-                        <th className="px-4 py-2">Registration</th>
-                        <th className="px-4 py-2">Early Bird</th>
+                      <tr className="bg-zinc-100 text-[18px]">
+                        <th className="px-4 py-2">Date</th>
+                        <td className="px-4 py-2">{date}</td>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td className="px-4 py-2">Robo (School/College)</td>
-                        <td className="px-4 py-2">12,000/-</td>
-                        <td className="px-4 py-2">6,000/-</td>
-                        <td className="px-4 py-2">1,500/-</td>
-                        <td className="px-4 py-2">1,000/-</td>
+                        <th className="px-4 py-2">Venue</th>
+                        <td className="px-4 py-2">{venue}</td>
                       </tr>
                       <tr className="bg-zinc-50">
-                        <td className="px-4 py-2">Robo (University)</td>
-                        <td className="px-4 py-2">20,000/-</td>
-                        <td className="px-4 py-2">15,000/-</td>
-                        <td className="px-4 py-2">2,000/-</td>
-                        <td className="px-4 py-2">1,500/-</td>
-                      </tr>
-                      <tr>
-                        <td className="px-4 py-2">Sumo (Sumo Wrestler Robot)</td>
-                        <td className="px-4 py-2">20,000/-</td>
-                        <td className="px-4 py-2">10,000/-</td>
-                        <td className="px-4 py-2">3,000/-</td>
-                        <td className="px-4 py-2">2,500/-</td>
-                      </tr>
-                      <tr className="bg-zinc-50">
-                        <td className="px-4 py-2">Off-Road</td>
-                        <td className="px-4 py-2">15,000/-</td>
-                        <td className="px-4 py-2">8,000/-</td>
-                        <td className="px-4 py-2">1,500/-</td>
-                        <td className="px-4 py-2">1,200/-</td>
+                        <th className="px-4 py-2">Fee:</th>
+                        <td className="px-4 py-2">{ticket}</td>
                       </tr>
                     </tbody>
                   </table>
@@ -61,7 +44,7 @@ export const Popup = ({ details, closeModal }) => {
               </div>
 
             </div>
-            <div className="mt-4  mr-4 text-right">
+            <div className="mt-4 mr-6 text-right">
               <button id='closemodal'
                 className="bg-[#E74C3C] text-[#FFFFFF] text-[16px] border-2 border-solid font-bold border-[#E74C3C] py-2 px-4 rounded"
                 onClick={closeModal}  >
