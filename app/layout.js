031c9c3c113b336aaa0465f5075db886d1111d79
devahmedfarooq <ahmedfarooq.dev@gmail.com>
2024-05-28 03:40:30 -0700

    ..

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import Head from "next/head";
 
 export const metadata = {
   title: "MustCon'24",
-  description: "Webiste developed for mustcon'24. The development team inculding Ahmed Farooq (Lead) and Asim (Co-Lead)",
+  description: "Webiste developed for mustcon'24.",
 };
 
 export default function RootLayout({ children }) {
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+  
       </Head>
 
       <body className={"scroll-smooth  " + inter.className}>
